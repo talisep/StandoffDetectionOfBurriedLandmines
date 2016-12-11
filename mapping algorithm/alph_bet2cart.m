8e4757082d44807182e07b55401839edfe4fbed3
talisep <tali.septon@gmail.com>
2016-12-11 14:36:11 +0200

    Delete alph_bet2cart.m

--- mapping algorithm/alph_bet2cart.m
+++
@@ -1,8 +0,0 @@
-function [x,y,z] = alph_bet2cart(beta,alpha,r)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes h
-x = r.*cos(beta);
-y=r.*cos(alpha).*sin(beta);
-z =r.*sin(alpha).*sin(beta); 
-end
-
